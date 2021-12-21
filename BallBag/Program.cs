6b780c4696f257ef22e6f9873c3cbd5e958497c2
Tianming Wu <tianming_y_wu@hotmail.com>
2021-12-20 22:11:54 -0600

    implemented check if bag weights meet target

--- BallBag/Program.cs
+++ BallBag/Program.cs
@@ -8,25 +8,17 @@ namespace BallBag
     {
         static void Main(string[] args)
         {
-            List<List<int>> test = new List<List<int>> { new List<int> { } };
-            Console.WriteLine(test.Count);
-            foreach (var item in test)
-            {
-                Console.WriteLine(item);
-            }
-
-            Console.WriteLine("test ends");
-            //Static
-            int NumberOfBalls = 10;
+            int NumberOfBalls = 6;
             int NumberOfBags = 3;
+            int TargetWeight = 7;
 
-            // setup a Ball
+            // setup ball: has weight of 1, 2, 3...
             int[] balls = new int[NumberOfBalls];
             for (int i = 0; i < NumberOfBalls; i++)
             {
                 balls[i] = i + 1;
             }
-            // setup a Bag representation Bags
+            // setup bag: an identity matrix to model which bag the ball gets thrown into;
             int[][] bags = new int[NumberOfBags][];
             for (int i = 0; i < NumberOfBags; i++)
             {
@@ -46,15 +38,25 @@ namespace BallBag
             int c = 1;
             foreach (List<int> t in throws)
             {
-                Console.WriteLine($"Configuration {c}:");
-                t.ForEach(Console.Write);
-                Console.Write("\n");
-                c++;
+                int[] BagWeights = new int[NumberOfBags];
+
+                for (int i=0; i<t.Count; i++)
+                {
+                    int bag = i % NumberOfBags;
+                    BagWeights[bag] += t[i];
+                }
+
+                if (BagWeights.All(x => x == TargetWeight))
+                {
+                    Console.WriteLine($"Configuration {c}:");
+                    t.ForEach(Console.Write);
+                    Console.Write("\n");
+                    c++;
+                }
             }
 
             Console.ReadKey();
         }
-
         public static List<List<int>> ThrowBall(List<List<int>> currentThrows, int[][] bags, int NumberOfBags, int BallWeight)
         {
             List<List<int>> newThrows = new List<List<int>>();
